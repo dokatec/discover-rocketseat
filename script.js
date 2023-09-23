06aa419bcdcfb65887de4889b0cfe1c265a58ec1
Jhonatan Pacheco da Silva <jhonatan.tec@gmail.com>
2023-09-23 15:40:09 -0300

    Evento onclick toggle javascript

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+function toggleMode() {
+    const html = document.documentElement;
+    html.classList.toggle('light');
+
+
+    // pegar a tag img
+    const img = document.querySelector('#profile img');
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/avatar-light.png')
+
+    } else {
+        img.setAttribute('src', './assets/avatar.png')
+    }
+
+
+}
+
+
+
